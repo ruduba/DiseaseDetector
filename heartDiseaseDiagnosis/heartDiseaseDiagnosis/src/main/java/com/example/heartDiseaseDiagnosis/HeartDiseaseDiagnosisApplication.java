aee8dae0b1d3d3085e0be8c2585d58daa7b21ba8
vatsala budur <114461494+ruduba@users.noreply.github.com>
2025-05-02 08:45:06 +0530

    First commit by 1MJ22CS203

---
+++ heartDiseaseDiagnosis/heartDiseaseDiagnosis/src/main/java/com/example/heartDiseaseDiagnosis/HeartDiseaseDiagnosisApplication.java
@@ -0,0 +1,13 @@
+package com.example.heartDiseaseDiagnosis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartDiseaseDiagnosisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartDiseaseDiagnosisApplication.class, args);
+	}
+
+}
